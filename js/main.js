d5397afd287bf49aaafaba096053d682725784af
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-23 14:55:57 +0900

    配列 #5

--- js/main.js
+++ js/main.js
@@ -1,54 +1,9 @@
 'use strict';
 
-// スコープ
-const x = 2;         // グローバススコープ
-function f() {
-    const x = 1;
-    console.log(x);  // 1
+{
+    const scores = [80, 90, 40];
+
+    scores[2] = 44; // constでも配列のvalueへの代入は可能
+    console.log(scores);
+    console.log(scores.length);
 }
-f();
-console.log(x);      // 2
-
-
-// アロー関数
-// 引数が1つの場合は()を省略可能
-const double = a => a * 2;
-console.log(double(12));
-
-const sum = (a, b, c) => a + b + c;
-// 関数式
-// const sum = function (a, b, c) {
-//     return a + b + c;
-// };
-const total = sum(1, 2, 3) + sum(4, 5, 6);
-console.log(total);
-
-
-// boolean
-// false <- 0 , null, undefined, '', false
-// true  <- それ以外
-
-// 数字からなる文字列
-console.log('5' * 3);                   // 15
-console.log('5' - '3');                 //  2
-
-console.log('5' + 3);                   // 53
-console.log(parseInt('5', 10) + 3);     //  8
-
-console.log(parseInt('hello', 10) + 3); //  NaN (Not a number)
-
-
-// 型
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-// nullのtypeがobjectになるのはJSの有名なバグ
-console.log(typeof null);
-
-
-// 'の利用
-console.log('it\'s me');
-console.log("it's me");
-// 改行、タブ
-console.log('Hel\nlo Wo\trld');
