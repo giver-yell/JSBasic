4271ef2008b2fd4c5ed4deb2812195b8b713027a
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-24 09:29:26 +0900

    map #5

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@
 'use strict';
 
 {
+    // map 配列に処理を加えて配列へ格納
+    const prices = [180, 190, 200];
+    const updatedPrices = prices.map(price => price + 20);
+    console.log(updatedPrices);
+
     // スプレッド構文
     const otherScores = [10, 20];
     const scores = [80, ...otherScores];
