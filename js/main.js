ef40abdb4c4a5f31cc46e49d2e83b3ccbf608b26
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-27 08:47:51 +0900

    タイマー機能 setInterval, setTimeout #5

--- js/main.js
+++ js/main.js
@@ -4,8 +4,35 @@
 'use strict'
 
 {
-    alert('hello');
+    // タイマー機能 setInterval, setTimeout
+    let i = 0;
 
+    // function  showTime() {
+    //     console.log(new Date());
+    //     i++;
+    //     if (i > 2) {
+    //         clearInterval(intervalId);
+    //     }
+    // }
+
+    // // 関数を引数にする場合は()つけない
+    // const intervalId = setInterval(showTime, 1000);
+
+    function showTime() {
+        console.log(new Date());
+        // 1度だけ実行
+        const timeoutId = setTimeout(showTime, 1000);
+        i++
+        if (i > 2) {
+            clearTimeout(timeoutId);
+        }
+    }
+    showTime();
+
+
+
+    // alert, confirm
+    alert('hello');
 
     const answer = confirm('削除しますか？');
     if (answer) {
