ddaf6096e69c68f5687e6483fe810b8ae0b369d8
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-26 10:15:39 +0900

    数値 #5

--- js/main.js
+++ js/main.js
@@ -2,5 +2,26 @@
 
 // 数値
 {
+    const scores = [10, 3, 9];
+    let sum = 0;
+
+    scores.forEach(score => {
+        sum += score;
+    });
+    const avg = sum / scores.length;
+    console.log(sum); // 22
+    console.log(avg); // 7.3333
+
+    // 切り捨て
+    console.log(Math.floor(avg)); // 7
+    // 切り上げ
+    console.log(Math.ceil(avg));  // 8
+    // 四捨五入
+    console.log(Math.round(avg)); // 7
+    // 小数点第三位まで表示
+    console.log(avg.toFixed(3));  // 7.333
+
+    // 数値をランダムで生成(ゲーム制作で必須)
+    console.log(Math.random());
 
 }
