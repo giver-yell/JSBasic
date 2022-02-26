f220212110787c8982690078f07b90c17f9bf690
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-26 14:47:22 +0900

    alert, confirm #5

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+ *
+*/
+'use strict'
+
+{
+    alert('hello');
+
+
+    const answer = confirm('削除しますか？');
+    if (answer) {
+        console.log('削除されました。');
+    } else {
+        console.log('キャンセルされました。');
+    }
+}
