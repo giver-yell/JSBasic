e5ad5fab412bbe362a0f0de9035551cf5e0d34e6
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-28 08:56:35 +0900

    focus, blur, input, change #7

--- js/main.js
+++ js/main.js
@@ -1,14 +1,44 @@
 'use strict'
 
 {
+    const text = document.querySelector('textarea');
+
+    // focus
+    text.addEventListener('focus', () => {
+        console.log('focus');
+    })
+
+    // blur(focusを外す)
+    text.addEventListener('blur', () => {
+        console.log('blur');
+    })
+
+    // input
+    text.addEventListener('input', () => {
+        // console.log('input');
+        console.log(text.value.length);
+    })
+
+    // change(input確定)
+    text.addEventListener('change', () => {
+        console.log('change');
+    })
+
+
+
+
+    // ダブルクリック
     document.querySelector('button').addEventListener('dblclick', () => {
         console.log('Double click!');
     })
 
+    // マウス操作
     // document.addEventListener('mousemove', e => {
     //     console.log(e.clientX, e.clientY);
     // });
-    document.addEventListener('keydown', e => {
-        console.log(e.key);
-    });
+
+    // キーボード操作
+    // document.addEventListener('keydown', e => {
+    //     console.log(e.key);
+    // });
 }
