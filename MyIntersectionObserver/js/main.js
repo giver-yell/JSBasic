ea2f26d2c0658ec0a2111a785ae46fca5f85a923
Ryohei Fujihara <giver.yell@gmail.com>
2022-03-01 07:42:15 +0900

    isIntersecting #9

--- MyIntersectionObserver/js/main.js
+++ MyIntersectionObserver/js/main.js
@@ -3,13 +3,27 @@
 {
     const target = document.querySelector('img');
 
-    function callback() {
-        console.log('fired!');
+    function callback(entries) {
+        console.log(entries[0]);
+
+        // アニメーションで消さない場合
+        if (!entries[0].isIntersecting) {
+            return;
+        }
+        entries[0].target.classList.add('appear');
+
+        // アニメーションで消す場合
+        // if (entries[0].isIntersecting) {
+        //     entries[0].target.classList.add('appear');
+        // } else {
+        //     entries[0].target.classList.remove('appear');
+        // }
     }
 
     const options = {
         // imgの表示 % を指定
-        threshold: [0.2, 0.8],
+        // threshold: [0.2, 0.8],
+        threshold: 0.2,
     }
 
     const observer = new IntersectionObserver(callback, options);
