90a56b2e081f2428f88c604c4d10f1bd4dcae352
Ryohei Fujihara <giver.yell@gmail.com>
2022-03-01 07:46:46 +0900

    rootMargin #9

--- MyIntersectionObserver/js/main.js
+++ MyIntersectionObserver/js/main.js
@@ -23,7 +23,8 @@
     const options = {
         // imgの表示 % を指定
         // threshold: [0.2, 0.8],
-        threshold: 0.2,
+        threshold: 1,
+        rootMargin: '0px 0px -100px',
     }
 
     const observer = new IntersectionObserver(callback, options);
